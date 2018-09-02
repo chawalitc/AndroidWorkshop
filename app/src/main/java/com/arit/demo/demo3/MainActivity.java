16c41daf8d45aee14b439d2549f66c072e016795
chawalitc <chawalitc>
2018-09-02 15:25:22 +0700

    first commit

---
+++ app/src/main/java/com/arit/demo/demo3/MainActivity.java
@@ -0,0 +1,31 @@
+package com.arit.demo.demo3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvMessage) TextView tvMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnHello)
+    public void doClickHello(){
+        tvMessage.setText(R.string.helloMessage);
+    }
+
+}
