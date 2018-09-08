77697237b5267c81c8987770254efdbe2822df1d
chawalitc <golfcssp2@gmail.com>
2018-09-08 10:23:49 +0700

    calculate BMI app

--- app/src/main/java/com/arit/demo/demo3/MainActivity.java
+++ app/src/main/java/com/arit/demo/demo3/MainActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.math.BigDecimal;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,6 +18,8 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.tvMessage) TextView tvMessage;
+    @BindView(R.id.etHeight) EditText etHeight;
+    @BindView(R.id.etWelght) EditText etWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,16 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnHello)
     public void doClickHello(){
-        tvMessage.setText(R.string.helloMessage);
+        BigDecimal height = new BigDecimal(etHeight.getText().toString());
+        BigDecimal weight = new BigDecimal(etWeight.getText().toString());
+        BigDecimal meter = new BigDecimal(100);
+        System.out.println("weight:"+weight+" height"+height);
+
+        BigDecimal bmiResult = new BigDecimal(0);
+        bmiResult = (height.divide(meter)).multiply((height.divide(meter)));
+        bmiResult = weight.divide(bmiResult, 2, BigDecimal.ROUND_HALF_UP);
+        System.out.println("bmiResult:"+bmiResult);
+        tvMessage.setText("Your BMI:"+bmiResult.toString());
     }
 
 }
